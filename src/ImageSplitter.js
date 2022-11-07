b2810c3baa40ba4ea8eb87ac499f5b47aa6aaf93
Hamas Ahmed <hammas.alphasquad@gmail.com>
2022-11-07 13:43:25 +0500

    Image Puzzel Updated

--- src/ImageSplitter.js
+++ src/ImageSplitter.js
@@ -10,20 +10,24 @@ export const ImageSplitter = () => {
     const imgArr = [
         {
             id: 1,
-            img: 'https://media.istockphoto.com/id/1354441996/photo/image-of-open-antique-book-on-wooden-table-with-glitter-overlay.jpg?b=1&s=170667a&w=0&k=20&c=O_VZbgONe4WTXPOEvwKYezhqFkzAXpr2g-lCdpdj5FU=',
+            img: 'https://pixabay.com/get/gfe44f948f47da3a3749bd009fa26f254128dfaa5cffa411585ed061fd2d4800ac5a1f097dc6e270ccddd801dfb0f8cecf17e4f1c2e74e78aa7e3f20baaa57658c56d77dae041d95e813842b9aaf6d276_640.jpg',
         },
         {
             id: 2,
-            img: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
+            img: 'https://pixabay.com/get/g286833db14e433a9fbdb589a14381c13ce723c1004301ffe0934642f0a91057818b93c42df8a9e148f5facd5f9c65f43b95c164cd0c4997f2e3d50fd06e011c505671d6c72d0b0008977882c36d6f560_640.jpg',
 
         },
         {
             id: 3,
-            img: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/58025/right-front-three-quarter.jpeg?q=75',
+            img: 'https://pixabay.com/get/g03758e112c08e3c27524a0865c3871e4224b3b1baa170dd41645661d8135034034f080762fd8f5ef351810779141d96199325fbae71e8a265874663b86da532c1254089b7d077ddcae44079b1cc89efb_640.jpg',
         },
         {
             id: 4,
-            img: 'https://media.istockphoto.com/id/535695503/photo/pakistan-monument-islamabad.jpg?s=612x612&w=0&k=20&c=bNqjdf8L-5igcRB89DdMgx0kNOmyeo1J_zzXmoxxl8w='
+            img: 'https://pixabay.com/get/ga03001c34d2146c8d05415fe1348bd7ee94235fff7b3c92ba326a9b911a2bfd6b606c609942793d243e643bbdb4a6d7f4762c385abb5f204c87bea728c1a6ac2656cdbe865b7075dfbe970556df8b6ac_640.jpg'
+        },
+        {
+            id: 5,
+            img: 'https://pixabay.com/get/g2cea037a912475cfd8202ad6436f3cffaec1f58344746db584e14d2b3e32e9d70533e2c51f54b83f9b92eb38c3bc311568293a3e7670f8fa504fa0655875bf214b7caaee4ba69b8f66c4488c741665e9_640.jpg'
         },
 
 
@@ -44,7 +48,7 @@ export const ImageSplitter = () => {
     let mouse;
     function images() {
         setImg('')
-        let randomNu = Math.floor(Math.random() * 4) + 1
+        let randomNu = Math.floor(Math.random() * 5) + 1
         let i = imgArr.find((it) => {
             if (it.id == randomNu) {
                 return it
@@ -59,8 +63,6 @@ export const ImageSplitter = () => {
             setInput('')
             setImg('')
             setDisAble(false)
-            // JSON.stringify(localStorage.removeItem('seconds', seconds))
-            // JSON.stringify(localStorage.removeItem('min', minutes))
             document.getElementById('canvas').classList.add('kk')
         }
     }, [input])
@@ -72,14 +74,12 @@ export const ImageSplitter = () => {
         window.location.replace('/')
         document.getElementById('canvas').classList.add('kk')
     }
-
     React.useEffect(() => {
         let interval;
         if (seconds > 59) {
-            setSeconds(0)
-            seconds = 0
-            setMinutes(pre => pre + 1)
             clearInterval(interval);
+            setSeconds(0)
+            setMinutes(pre => pre + 1)
         }
         if (isActive) {
             interval = setInterval(() => {
@@ -87,12 +87,9 @@ export const ImageSplitter = () => {
             }, 1000);
         } else if (!isActive && seconds !== 0) {
             clearInterval(interval);
-            setSeconds(0)
-            setMinutes(0)
-
         }
         return () => clearInterval(interval);
-    }, [isActive]);
+    }, [isActive, seconds]);
     React.useEffect(() => {
         img.src = ''
         images()
@@ -368,7 +365,7 @@ export const ImageSplitter = () => {
                 if (gameWin) {
                     setTimeout(gameOver, 500);
                     setTimeout(() => {
-                        alert('WellCome to the AK team')
+                        alert('WelCome to the AK team')
                     }, 600);
                     setSeconds(pre => localStorage.setItem('seconds', JSON.stringify(pre)))
                     setMinutes(pre => localStorage.setItem('min', JSON.stringify(pre)))
@@ -409,7 +406,7 @@ export const ImageSplitter = () => {
                     Last Time (0{localStorage.getItem('min')}m:{localStorage.getItem('seconds') >= 10 ? localStorage.getItem('seconds') : '0'.concat(localStorage.getItem('seconds'))}s)
                 </div>
             }
-            <h1>Puzzel Game</h1>
+            <h1>Puzzle Game</h1>
             <h3>Put Any number from 2 to 5</h3>
             <form onSubmit={handle} style={{ margin: '20px 0' }}>
                 <input className="input" type="number" value={input} onChange={(e) => {
@@ -439,7 +436,10 @@ export const ImageSplitter = () => {
                                 }}>Start The Puzzel</button>
                                 <button className='btn btn2' onClick={reset}>Reset</button>
                             </>
-                            : <p className='btn btn2'>Time  0{minutes.toString()}:{seconds > 9 ? seconds.toString() : '0'.concat(seconds.toString())}</p>)
+                            : <>
+                                <p className='btn btn2'>Time  0{minutes.toString()}:{seconds > 9 ? seconds.toString() : '0'.concat(seconds.toString())}</p>
+                                <button className='btn btn2' onClick={reset}>Reset</button>
+                            </>)
                     }
                 </div>
             </div>
