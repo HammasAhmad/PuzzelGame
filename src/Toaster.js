6d6f541faac8f698bced6fd288f021a306755d25
Hamas Ahmed <hammas.alphasquad@gmail.com>
2022-11-24 18:46:00 +0500

    'update'

--- src/Toaster.js
+++ src/Toaster.js
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function Toaster({ toaster }) {
-    const notify = () => toast("Congrats You Win!");
+    const notify = () => toast("WellCome TO AK's Team");
     let winner = new Audio('./music/winner.mp3')
 
     React.useEffect(() => {
